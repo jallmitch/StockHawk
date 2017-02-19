6f3c78ae557e40d93108716d3daa8c3a6ebb31d0
Jesse Mitchell <jallmitch@>
2017-02-18 19:48:45 -0600

    Added default files to support a widget.

---
+++ app/src/main/java/com/udacity/stockhawk/widget/StockQuoteWidgetProvider.java
@@ -0,0 +1,10 @@
+package com.udacity.stockhawk.widget;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by jesse.mitchell on 2/18/2017.
+ */
+
+public class StockQuoteWidgetProvider extends AppWidgetProvider {
+}
