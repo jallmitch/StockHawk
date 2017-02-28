c033f21fa4d57a9bdbcd4fd2c0c9dcaf1b9fc078
Jesse Mitchell <jallmitch@tamu.edu>
2017-02-27 21:37:11 -0600

    Removed the chart and returned stockQuotes size.

--- app/src/main/java/com/udacity/stockhawk/widget/StockQuoteWidgetProvider.java
+++ app/src/main/java/com/udacity/stockhawk/widget/StockQuoteWidgetProvider.java
@@ -21,6 +21,12 @@ public class StockQuoteWidgetProvider extends AppWidgetProvider {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        AppWidgetManager mgr = AppWidgetManager.getInstance(context);
+        if (intent.getAction().equals(STOCK_QUOTE))
+        {
+            int appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+            int viewIndex = intent.getIntExtra("", 0);
+        }
         super.onReceive(context, intent);
     }
 
@@ -41,24 +47,24 @@ public class StockQuoteWidgetProvider extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        for (int appWidgetId : appWidgetIds) {
+        for (int i = 0; i < appWidgetIds.length; i++) {
             RemoteViews widget = new RemoteViews(context.getPackageName(), R.layout.stock_widget);
 
             Intent serviceIntent = new Intent(context, StockWidgetService.class);
-            serviceIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+            serviceIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[i]);
             serviceIntent.setData(Uri.parse(serviceIntent.toUri(Intent.URI_INTENT_SCHEME)));
-            widget.setRemoteAdapter(appWidgetId, serviceIntent);
+            widget.setRemoteAdapter(appWidgetIds[i], R.id.stock_widget, serviceIntent);
             widget.setEmptyView(R.id.stock_widget, R.id.empty_view);
 
             Intent intent = new Intent(context, StockDetailFragment.class);
-            intent.setAction("");
-            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+            intent.setAction(STOCK_QUOTE);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[i]);
             intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
 
             PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
             widget.setPendingIntentTemplate(R.id.stock_widget, pendingIntent);
 
-            appWidgetManager.updateAppWidget(appWidgetId, widget);
+            appWidgetManager.updateAppWidget(appWidgetIds[i], widget);
         }
         super.onUpdate(context, appWidgetManager, appWidgetIds);
     }
